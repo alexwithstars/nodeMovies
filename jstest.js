cfd4d2d96a918cc66e29c93b75213fd952441f7e
alexwithstars <rayo080378@gmail.com>
2024-08-08 23:32:13 -0600

    trash

--- jstest.js
+++
@@ -1,6 +0,0 @@
-import {encryptStr,getDictionary,decryptStr} from "./utils/encrypt.js"	
-const base = getDictionary("1")
-const chr = n=>String.fromCodePoint(n)
-const ord = n=>n.codePointAt(0)
-
-console.log(ord(""))
